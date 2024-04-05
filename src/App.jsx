78145931b2810ff37b151413a57a3e83e713d01d
Sourav Dutta <souravduttasd2@gmail.com>
2024-04-05 16:48:53 +0530

    checkeng pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>{import.meta.env.VITE_APP_NAME}</p>
+        <p>This is the testing the pipeline</p>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
